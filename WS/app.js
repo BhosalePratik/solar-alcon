06d67197a6b815d3b43845ea531e3464542f2466
Pratik <pikachubhosale96@gmail.com>
2023-07-26 16:12:19 +0530

    created error logger middleware

--- WS/app.js
+++ WS/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
 var database = require('./database');
+var requestLogger = require('./public/javascripts/RequestLogger');
+var errorLogger = require('./public/javascripts/ErrorLogger');
+app.use(requestLogger);
+app.use(errorLogger);
 app.listen(3050);
 console.log("Server started at port 3050!");
